02c0b0a5d7b56d35140e3e4dac834cad06791587
Arjun Virmani <72364512+arjunvirmani7@users.noreply.github.com>
2024-03-03 13:59:32 +1300

    Add files via upload

---
+++ C_02_Instructions.py
@@ -0,0 +1,25 @@
+# Generates headings (eg: ---- Heading ----)
+
+def statement_generator(statement, decoration):
+    print(f"\n{decoration * 5} {statement} {decoration * 5}")
+
+
+# Dispays instructions
+def instructions():
+    statement_generator("Instructions", "-")
+
+    print('''
+Instructions go here.
+- instruction 1
+- instruction 2    
+- etc  
+    ''')
+
+# Main routine goes here
+want_instructions = input("Press <enter> to read the instructions or any key to continue")
+
+# Display instructions if requested
+if want_instructions == "":
+    instructions()
+
+print("program continues")
